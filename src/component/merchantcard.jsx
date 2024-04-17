793a900484e2b5f99021f2607d144a88e5c87b87
iyiolakeni <iyiolakeni@gmail.com>
2024-04-17 14:16:38 +0100

    Final Styling

--- src/component/merchantcard.jsx
+++ src/component/merchantcard.jsx
@@ -3,7 +3,12 @@ import axios from 'axios';
 
 const MerchantCard = ({merchantID}) => {
     const [merchant, setMerchant] = useState(null);
-    
+    const navItems = ["Basic Information", "Contact"];
+
+    const [selectedForm, setSelectedForm] = useState("form1");
+    const handleOnClick = (formId) => {
+      setSelectedForm(formId);
+    };
     //This function fetches the request details from the server and sets them in state.
     useEffect(() => {
         const fetchRequest = async () => {
@@ -23,65 +28,83 @@ const MerchantCard = ({merchantID}) => {
     
     return (
         <div className="review">
-        <div className="reviewForm">
+          <nav>
+  <ul className="ordernav">
+    {navItems.map((item, index) => {
+      let formId = `form${index + 1}`;
+      return (
+        <li
+          key={index}
+          className={selectedForm === formId ? 'active' : ''}
+          onClick={() => handleOnClick(formId)}
+        >
+          {item}
+        </li>
+      );
+    })}
+  </ul>
+</nav>
+        {selectedForm === "form1" && (<div className="reviewForm">
             <label htmlFor=""> Merchant ID:
             </label>
-            <textarea value={merchant.MerchantID} readonly class="reviewForm-output" />
+            <p >{merchant.MerchantID}</p>
             <label> Merchant Name:
             </label>
-              <textarea value={merchant.Merchant_Trade_Name} readonly class="reviewForm-output" />
+              <p >{merchant.Merchant_Trade_Name}</p>
             <label> Business Type: 
             </label>
-            <textarea value={merchant.Business_type} readonly class="reviewForm-output" />
+            <p >{merchant.Business_type}</p>
             <label> Business Location:
             </label>
-            <textarea value={merchant.Business_location} readonly class="reviewForm-output" />
+            <p >{merchant.Business_location}</p>
             <label> RC Number:
             </label>
-            <textarea value={merchant.RC_Number} readonly class="reviewForm-output" />
+            <p >{merchant.RC_Number}</p>
             <label> Number of Branches: 
             </label>
-            <textarea value={merchant.No_of_branches} readonly class="reviewForm-output" />
+            <p >{merchant.No_of_branches}</p>
             <label> Opening Hours: 
             </label>
-            <textarea value={merchant.opening_hours} readonly class="reviewForm-output" />
+            <p >{merchant.opening_hours}</p>
             <label> Website:
             </label>
-            <textarea value={merchant.website} readonly class="reviewForm-output" />
+            <p >{merchant.website}</p>
             <label> Address:
             </label>
-            <textarea value={merchant.Office_address} readonly class="reviewForm-output" />
+            <p >{merchant.Office_address}</p>
         </div>
+        )}
+        {selectedForm === "form2" && (
         <div className="reviewForm">
             <label htmlFor=""> LGA:
             </label>
-            <textarea value={merchant.LGA} readonly class="reviewForm-output" />
+            <p>{merchant.LGA}</p>
             <label> State:
             </label>
-            <textarea value={merchant.state} readonly class="reviewForm-output" />                  
+            <p>{merchant.state}</p>                  
             <label> Primary Contact Name: 
             </label>
-            <textarea value={merchant.Name_of_Primary_Contact} readonly class="reviewForm-output" />
+            <p>{merchant.Name_of_Primary_Contact}</p>
             <label> Primary Contact Phone Number:
             </label>
-            <textarea value={merchant.office_No} readonly class="reviewForm-output" />
+            <p>{merchant.office_No}</p>
             <label> Primary Contact Email:
             </label>
-            <textarea value={merchant.office_email} readonly class="reviewForm-output" />
+            <p>{merchant.office_email}</p>
             <label> Primary Contact Designation: 
             </label>
-            <textarea value={merchant.Designation} readonly class="reviewForm-output" />
+            <p>{merchant.Designation}</p>
             <label> Secondary Contact Name:
             </label>
-            <textarea value={merchant.Name_of_Secondary_Contact} readonly class="reviewForm-output" />
-            <textarea value={merchant.Business_type} readonly class="reviewForm-output" />
+            <p>{merchant.Name_of_Secondary_Contact}</p>
             <label> Secondary Contact Phone Number:
             </label>
-            <textarea value={merchant.office_No2} readonly class="reviewForm-output" />
+            <p>{merchant.office_No2}</p>
             <label> Secondary Contact Email:
             </label>
-            <textarea value={merchant.office_email2} readonly class="reviewForm-output" />
+            <p>{merchant.office_email2}</p>
         </div>
+        )}
     </div>
     );
 }
