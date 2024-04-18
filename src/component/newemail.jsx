c428749ed77801452b776b31da7d563e335dccc9
Lupinogbodo <lupinogbodo22@gmail.com>
2024-04-18 10:58:38 +0100

    download

--- src/component/newemail.jsx
+++ src/component/newemail.jsx
@@ -45,7 +45,7 @@ const NewEmail = (props) => {
         <textarea value={body} onChange={(e) => setBody(e.target.value)} required />
       </label>
       <button type="submit">Send</button>
-      <button type='close' onClick={() => setOpen(false)}>Close</button>
+      <button type='close'>Close</button>
     </form>
   );
 };
