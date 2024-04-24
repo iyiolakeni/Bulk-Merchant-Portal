87d0fba89bcbd9a2f869b8a47724d05b2cec06ac
iyiolakeni <iyiolakeni@gmail.com>
2024-04-24 10:08:54 +0100

    Working Email

--- src/component/newemail.jsx
+++ src/component/newemail.jsx
@@ -1,15 +1,11 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useRef } from 'react';
 import axios from 'axios';
 import { UserContext } from "../UserContext";
 
 
 const NewEmail = (props) => {
-  const {user} = useContext(UserContext)
-  const [recipient, setRecipient] = useState('');
-  const [subject, setSubject] = useState('');
-  const [body, setBody] = useState('');
-  const sender = user.email;
-  // const [open, setOpen] = useState(false);
+  const {user} = useContext(UserContext);
+  const formObj =useRef();
 
   const handleClose = () => {
     props.closeForm();
@@ -18,17 +14,20 @@ const NewEmail = (props) => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    
+    const form = new FormData(formObj.current);
+    const email = {};
+    form.forEach((value, key) => {
+      email[key] = value;
+    });
 
-    const email = {
-      sender,
-      recipient,
-      subject,
-      body,
-    };
+    //get User email
+    email.from = user.email;
 
     try {
       const response = await axios.post('http://localhost:5000/emails/send', email);
       console.log(response.data);
+      formObj.current.reset();
       props.closeForm();
     } catch (error) {
       console.error(error);
@@ -36,18 +35,18 @@ const NewEmail = (props) => {
   };
 
   return (
-    <form className='emailForm' onSubmit={handleSubmit}>
+    <form ref={formObj} className='emailForm' onSubmit={handleSubmit}>
       <label>
         To:
-        <input type="text" value={recipient} onChange={(e) => setRecipient(e.target.value)} required />
+        <input type="text" name='to' required />
       </label>
       <label>
         Subject:
-        <input type="text" value={subject} onChange={(e) => setSubject(e.target.value)} required />
+        <input type="text" name='subject' required />
       </label>
       <label>
         Body:
-        <textarea value={body} onChange={(e) => setBody(e.target.value)} required />
+        <textarea name='message' required />
       </label>
       <button type="submit">Send</button>
       <button type='close' onClick={handleClose}>Close</button>
